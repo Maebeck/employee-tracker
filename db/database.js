fef235519b8bc19325061a6974dbfe92a503a42c
Corey Smith <CSSmith1997@gmail.com>
2020-12-11 20:04:59 -0700

    Added the initial prompt to the user, the choices the user can make, and started the codes/functions for each choice available.

--- db/database.js
+++ db/database.js
@@ -13,3 +13,67 @@ const eSalary = (salary) => {
 };
 
 init ();
+
+async function init() {
+    await inquirer.prompt({
+    name: "Command",
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+        'View Employee',
+        'View Department',
+        'View Role',
+        'Add Employee',
+        'Add Department',
+        'Add Role',
+        'Update Employee',
+        'Update Department',
+        'Update Role',
+        'Delete Employee',
+        'Delete Department',
+        'Delete Role',
+    ]
+    }).then(async function (choice) {
+        switch (choice.Command) {
+            case 'View Employees':
+            employeeSelection('view');
+            break;
+            case 'View Departments':
+                view('dept');
+                break;
+            case 'View Roles':
+                view('role');
+                break;
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Update Employees':
+                employeeSelection('update');
+                break;
+            case 'Update Department':
+                updateDepartment();
+                break;
+            case 'Update Role':
+                updateRole();
+                break;
+            case 'Remove Employee':
+                employeeSelection('remove');
+                break;
+            case 'Remove Department':
+                removeRoleOrDept('dept');
+                break;
+            case 'Remove Role':
+                removeRoleOrDept('role');
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    });
+};
